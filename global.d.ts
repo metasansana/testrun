5f0e5aa376f9c3e661b0c1fdb23eb74df6da121d
Lasana Murray <lmurray@quenk.com>
2020-05-11 20:40:57 -0400

    chore: move globals to root of project

---
+++ global.d.ts
@@ -0,0 +1,49 @@
+declare var cloneInto: any;
+
+declare type Callback = (e?: Error | null, v?: any) => void;
+
+interface Window {
+
+    execCLIScript: Function,
+
+    cloneInto: any,
+
+    Mocha: {
+
+        reporters: {
+
+            Base: import('mocha').reporters
+
+        },
+
+        Runner: {
+
+            constants: {
+
+                EVENT_TEST_PASS: string,
+                EVENT_TEST_FAIL: string,
+                EVENT_TEST_END: string,
+                EVENT_RUN_END: string,
+                EVENT_TEST_PENDING: string
+
+            }
+
+        }
+
+    },
+
+    mocha: {
+
+        setup(opts?: Mocha.Interface | Mocha.MochaSetupOptions): Mocha
+
+        run(): Mocha.Runner
+
+    }
+
+}
+
+declare namespace browser {
+
+    interface tabs { }
+
+}
