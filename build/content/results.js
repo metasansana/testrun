962a129f06c803c13ae0be725936191ee033de51
Lasana Murray <lmurray@quenk.com>
2020-05-13 18:50:55 -0400

    chore: clean up

--- build/content/results.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(function () {
-    var URL_MOCHA_CSS = '/src/app/public/mocha.css';
-    browser.tabs.insertCSS({
-        file: URL_MOCHA_CSS
-    });
-})();
-//# sourceMappingURL=results.js.map
