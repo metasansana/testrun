a6534f72aab1c846a79bf6dedffc8dd4ada307f6
Lasana Murray <lmurray@quenk.com>
2020-05-14 13:11:30 -0400

    chore: rebuild

--- build/background/extension.js
+++ build/background/extension.js
@@ -1,9 +1,16 @@
 "use strict";
 var tab = -1;
+var url = '/src/app/public/index.html';
+var handleErr = function (e) {
+    alert('Error [B001]: Unable to create Testrun tab!');
+    console.error(e);
+};
 browser.browserAction.onClicked.addListener(function (t) {
     tab = t.id;
-    browser.tabs.create({
-        url: '/src/app/public/index.html',
+    browser.runtime.onConnect.addListener(function (p) {
+        p.postMessage({ id: t.id, type: 'testrun-target-tab' });
     });
+    var conf = { url: url };
+    browser.tabs.create(conf).catch(handleErr);
 });
 //# sourceMappingURL=extension.js.map
--- build/background/extension.ts
+++ build/background/extension.ts
@@ -1,14 +1,26 @@
 
 let tab: number = -1;
+let url = '/src/app/public/index.html';
+
+const handleErr = (e: Error) => {
+
+    alert('Error [B001]: Unable to create Testrun tab!');
+    console.error(e);
+
+}
 
 browser.browserAction.onClicked.addListener((t: browser.tabs.Tab) => {
 
     tab = <number>t.id;
 
-    browser.tabs.create({
+    browser.runtime.onConnect.addListener((p: browser.runtime.Port) => {
 
-        url: '/src/app/public/index.html',
+        p.postMessage({ id: t.id, type: 'testrun-target-tab' });
 
     });
+
+    let conf = { url };
+
+    browser.tabs.create(conf).catch(handleErr);
 
 });
