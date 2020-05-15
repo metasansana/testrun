f17f9d407f74ade897dc44b1233dceac6c28359c
Lasana Murray <lmurray@quenk.com>
2020-05-15 14:21:49 -0400

    refactor: add function for executing scripts

--- packages/testrun/lib/content/index.js
+++ packages/testrun/lib/content/index.js
@@ -2,4 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SCRIPT_INIT_TEST_ENV = '/build/content/initTestEnv.js';
 exports.SCRIPT_INIT_TEST_RESULT_ENV = '/build/content/initTestResultEnv.js';
+/**
+ * execContentScriptFile executes a content script using an extension file
+ * path.
+ *
+ * This function preloads the chrome polyfill before execution.
+ */
+exports.execContentScriptFile = function (id, file) {
+    return browser
+        .tabs
+        .executeScript(id, { file: '/vendor/browser-polyfill.js' })
+        .then(function () {
+        return browser
+            .tabs
+            .executeScript(id, { file: file });
+    })
+        .then(function () { return Promise.resolve(); });
+};
 //# sourceMappingURL=index.js.map
--- packages/testrun/src/content/index.ts
+++ packages/testrun/src/content/index.ts
@@ -1,3 +1,20 @@
 
 export const SCRIPT_INIT_TEST_ENV = '/build/content/initTestEnv.js';
 export const SCRIPT_INIT_TEST_RESULT_ENV = '/build/content/initTestResultEnv.js';
+
+/**
+ * execContentScriptFile executes a content script using an extension file
+ * path.
+ *
+ * This function preloads the chrome polyfill before execution.
+ */
+export const execContentScriptFile = (id: number, file: string)
+    : Promise<void> =>
+    browser
+        .tabs
+        .executeScript(id, { file: '/vendor/browser-polyfill.js' })
+        .then(() =>
+            browser
+                .tabs
+                .executeScript(id, { file }))
+        .then(() => Promise.resolve());
